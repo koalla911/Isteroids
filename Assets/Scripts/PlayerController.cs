5127f35c529b4edb1453f82867c1de47a6c9e8cc
Constance <koalla911@gmail.com>
2023-02-22 17:58:06 +0200

    basic player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Game
 {
+	public class PlayerController : MonoBehaviour
+	{
+		[SerializeField] private float speed = default;
+		[SerializeField] private float degrees = default;
+		[SerializeField] private SpriteRenderer flame = default;
+
+		private void Update()
+		{
+			if (flame)
+			{
+				flame.gameObject.SetActive(Input.GetKey(KeyCode.W));
+			}
+
+			if (Input.GetKey(KeyCode.W))
+			{
+				this.transform.position += speed * Time.deltaTime * transform.up;
+			}
+			else if (Input.GetKey(KeyCode.S))
+			{
+				this.transform.position += speed * Time.deltaTime * (-transform.up);
+			}
+
+			if (Input.GetKey(KeyCode.A))
+			{
+				this.transform.eulerAngles += degrees * Time.deltaTime * Vector3.forward;
+			}
+			else if (Input.GetKey(KeyCode.D))
+			{
+				this.transform.eulerAngles += degrees * Time.deltaTime * Vector3.back;
+			}
+		}
+	}
 }
